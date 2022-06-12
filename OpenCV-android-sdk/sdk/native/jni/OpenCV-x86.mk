adf4cb0b09564365938db4e7e2a7e4aca40a7853
Alcibiado <49393198+Alcibiado@users.noreply.github.com>
2022-06-12 15:04:37 +0800

    Aruco Android SDK编译完成

---
+++ OpenCV-android-sdk/sdk/native/jni/OpenCV-x86.mk
@@ -0,0 +1,2 @@
+OPENCV_3RDPARTY_COMPONENTS:=cpufeatures ittnotify libprotobuf tbb libjpeg-turbo libwebp libpng libtiff libjasper IlmImf quirc
+OPENCV_EXTRA_COMPONENTS:=ade D:/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/i686-linux-android/libz.a dl m log
---
+++ OpenCV-android-sdk/sdk/native/jni/OpenCV-x86_64.mk
@@ -0,0 +1,2 @@
+OPENCV_3RDPARTY_COMPONENTS:=cpufeatures ittnotify libprotobuf tbb libjpeg-turbo libwebp libpng libtiff libjasper IlmImf quirc
+OPENCV_EXTRA_COMPONENTS:=ade D:/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/x86_64-linux-android/libz.a dl m log
